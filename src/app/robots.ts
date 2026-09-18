bef9ee66702291bc550e92a755ba7117778e0c9c
nanjing734-coder <nanjing734@gmail.com>
2026-09-18 07:53:46 -0500

    Allow search crawlers to access Next assets

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -3,7 +3,7 @@ import { siteConfig } from "@/config/site";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: { userAgent: "*", allow: "/", disallow: ["/_next/", "/api/"] },
+    rules: { userAgent: "*", allow: "/", disallow: "/api/" },
     sitemap: `${siteConfig.url}/sitemap.xml`,
     host: siteConfig.url,
   };
